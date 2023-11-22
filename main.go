84cdda1a2a3f322a6f03008cade33e080991c250
Malcome Baggett <MalcomeBaggett@gmail.com>
2023-11-22 16:27:38 -0500

    built bare minimun of a basic server

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// Define a home handler function which writes a byte slice containing
+// "Hello from Snippetbox" as the response body.
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	// use the http.NewServerMux() function to initialize a new new servemux, then
+	// register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+
+	// use the http.HandleFunc() function to register the home() as the handler
+	// Two parameters: the tcp network address to listen on (in this case ":4000")
+	// and the servemux we just created. If http.ListenAndServe() returns an error
+	// we use log.fatal() function to log the error message and exit.
+	// note any error returned by http.ListenAndServe() is always non-nil.
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
