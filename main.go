ef41fdde67368d65faad8177321149047d07cf5c
Malcome Baggett <MalcomeBaggett@gmail.com>
2023-11-25 21:41:21 -0500

    Learn how to handle a post req

--- main.go
+++ main.go
@@ -8,7 +8,31 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		// Check if the current request URL path exactly matches "/". If it doesn't, use
+		// the http.NotFound() function to send a 404 response to the client.
+		// Importantly, we then return from the handler. If we don't return the handler
+		// would keep executing and also write the "Hello from SnippetBox" message.
+		http.NotFound(w, r)
+		return
+
+	}
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.method to check whether the request is using POST or not. Note that
+	if r.Method != http.MethodPost {
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+	w.Write(([]byte("Create a new snippet...")))
 }
 
 func main() {
@@ -16,6 +40,8 @@ func main() {
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 	// use the http.HandleFunc() function to register the home() as the handler
 	// Two parameters: the tcp network address to listen on (in this case ":4000")
 	// and the servemux we just created. If http.ListenAndServe() returns an error
